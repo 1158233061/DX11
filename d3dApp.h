da1bffe8178b5e54d5bfbd01d93ad12489667468
1158233061 <1158233061@qq.com>
2020-11-16 20:59:11 +0800

    [add] 添加文字显示

--- d3dApp.h
+++ d3dApp.h
@@ -3,6 +3,8 @@
 
 #include <wrl/client.h>
 #include <string>
+#include <d2d1.h>
+#include <dwrite.h>
 #include <d3d11_1.h>
 #include <DirectXMath.h>
 #include "Mouse.h"
@@ -10,6 +12,8 @@
 #include "GameTimer.h"
 
 // 添加所有要引用的库
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dxguid.lib")
@@ -28,7 +32,7 @@ public:
 
 	int Run();                                // 运行程序，进行游戏主循环
 
-	                                          // 框架方法。客户派生类需要重载这些方法以实现特定的应用需求
+	// 框架方法。客户派生类需要重载这些方法以实现特定的应用需求
 	virtual bool Init();                      // 该父类方法需要初始化窗口和Direct3D部分
 	virtual void OnResize();                  // 该父类方法需要在窗口大小变动的时候调用
 	virtual void UpdateScene(float dt) = 0;   // 子类需要实现该方法，完成每一帧的更新
@@ -37,6 +41,7 @@ public:
 	// 窗口的消息回调函数
 protected:
 	bool InitMainWindow();      // 窗口初始化
+	bool InitDirect2D();        // Direct2D初始化
 	bool InitDirect3D();        // Direct3D初始化
 
 	void CalculateFrameStats(); // 计算每秒帧数并在窗口显示
@@ -58,6 +63,10 @@ protected:
 	// 使用模板别名(C++11)简化类型名
 	template <class T>
 	using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// Direct2D
+	ComPtr<ID2D1Factory> m_pd2dFactory;					  // D2D工厂
+	ComPtr<ID2D1RenderTarget> m_pd2dRenderTarget;		  // D2D渲染目标
+	ComPtr<IDWriteFactory> m_pdwriteFactory;			  // DWrite工厂
 	// Direct3D 11
 	ComPtr<ID3D11Device> m_pd3dDevice;                    // D3D11设备
 	ComPtr<ID3D11DeviceContext> m_pd3dImmediateContext;   // D3D11设备上下文
